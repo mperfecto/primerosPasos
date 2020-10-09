2056abca84161d9149b9eab99322676f7f9f1297
mperfecto <mperfectoarjona@gmail.com>
2020-10-09 10:58:14 +0200

    primero pasos

---
+++ Sumador.java
@@ -0,0 +1,14 @@
+/**
+ * Dado
+ */
+public class Sumador {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 2;
+        int suma = num1 + num2;
+
+        System.out.println("La suma vale: "+suma);
+    }
+    
+}
