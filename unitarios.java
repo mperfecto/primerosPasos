2fe20916d8831ad180ed7eb6a11a3d0f6b5d01a8
Manuchei <72012109+Manuchei@users.noreply.github.com>
2020-10-26 12:26:59 +0100

    Add files via upload

---
+++ unitarios.java
@@ -0,0 +1,17 @@
+package datos1;
+
+public class unitarios {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int m=2, n=5;
+		m++; // m=3
+		System.out.println("m vale: "+m);
+		n--; // n=4
+		System.out.println("n vale: "+n);
+		
+		
+	}
+
+}
+
